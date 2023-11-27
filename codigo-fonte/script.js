d81929877c5a91d645ea7ef90944471d7937cacd
Victor Gabriel <118944175+flemys@users.noreply.github.com>
2023-11-26 22:29:34 -0300

    Add files via upload

---
+++ codigo-fonte/script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Adicionar Contas</title>
+</head>
+<body>
+    <h2>Adicionar Contas</h2>
+
+    <form id="formConta">
+        <label for="nomeUsuario">Nome de Usuário:</label>
+        <input type="text" id="nomeUsuario" required>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" required>
+        <button type="submit">Adicionar Conta</button>
+    </form>
